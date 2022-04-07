ba7081cd8d27f8f56d646a5a9982f40053978eec
ArshNaira <arshnaira@mail.ru>
2022-04-07 15:08:54 +0400

    createProject

---
+++ naira-arshakyan/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import PersonalData from './components/PersonalData';
+
+function App() {
+  return (<div>
+    
+    <PersonalData />
+    </div>
+  );
+}
+
+export default App;
